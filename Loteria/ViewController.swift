e135131bddbbc2bb880606e50ea61c448c2b84c3
Jonas <jonassilvajns@icloud.com>
2022-07-21 13:30:19 -0300

    Initial Commit

---
+++ Loteria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Jonas Silva on 21/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
